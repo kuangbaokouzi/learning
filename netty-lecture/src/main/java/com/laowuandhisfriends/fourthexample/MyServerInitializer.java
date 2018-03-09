c6ef8c0be5213c5f506637bca19b37753e835a6f
wubin <35773597@qq.com>
2018-03-09 13:47:16 +0800

    add a new example

---
+++ netty-lecture/src/main/java/com/laowuandhisfriends/fourthexample/MyServerInitializer.java
@@ -0,0 +1,24 @@
+package com.laowuandhisfriends.fourthexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.IdleStateHandler;
+import io.netty.util.CharsetUtil;
+
+import java.util.concurrent.TimeUnit;
+
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline channelPipeline = ch.pipeline();
+
+        // 空闲状态处理器，监控读、写、读写空闲
+        channelPipeline.addLast(new IdleStateHandler(5,7,10, TimeUnit.SECONDS));
+        channelPipeline.addLast(new MyServerHandler());
+    }
+}
